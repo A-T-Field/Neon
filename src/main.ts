040146f33a40b40e19d0c43d9a328e50bacbb85e
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-04-04 20:51:27 +0800

    fix :-1:

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,20 @@
  * @Author: maggot-code
  * @Date: 2022-03-26 21:15:17
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-04-04 19:28:33
+ * @LastEditTime: 2022-04-04 20:50:18
  * @Description: file content
  */
 import AppProxy from '@/layout/AppProxy/AppProxy.vue';
 import AppGlobal from '@/layout/AppGlobal/AppGlobal.vue';
 import { createApp } from 'vue';
-import { setupView } from '@/view';
-import { setupRouter } from '@/shared/route';
 import "normalize.css";
 
 async function main() {
     const appProxy = createApp(AppProxy);
     const appGlobal = createApp(AppGlobal);
 
-    setupView();
     appProxy.mount("#app-proxy", true);
 
-    await setupRouter(appGlobal);
     appGlobal.mount("#app", true);
 }
 
