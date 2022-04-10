fe51509d70bc3e3fa11b19ac8ffe60a016d562d2
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-04-10 22:08:24 +0800

    Update index.ts

--- src/shared/duty/useCase/createTask/index.ts
+++ src/shared/duty/useCase/createTask/index.ts
@@ -2,6 +2,18 @@
  * @Author: maggot-code
  * @Date: 2022-04-09 19:25:58
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-04-09 19:25:59
+ * @LastEditTime: 2022-04-10 22:08:21
  * @Description: file content
  */
+import { UseCase } from '@/shared/core/useCase';
+import {
+    DutyTaskName,
+    DutyTask
+} from '../../domain/task';
+
+export class CreateTask implements UseCase<any, any> {
+
+    execute(request?: any) {
+        throw new Error('Method not implemented.');
+    }
+}
