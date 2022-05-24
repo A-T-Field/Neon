b2154f18ed32de41a38569713881ffc324f6b1c1
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-05-24 20:39:18 +0800

    fix :update package version

--- src/infra/layouts/App.vue
+++ src/infra/layouts/App.vue
@@ -2,7 +2,7 @@
  * @Author: maggot-code
  * @Date: 2022-04-19 23:57:05
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-05-18 21:09:27
+ * @LastEditTime: 2022-05-24 20:30:19
  * @Description: file content
 -->
 <script setup lang='ts'></script>
