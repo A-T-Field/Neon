730ebd671512a1a051fa165e6c4f2ac6661d141c
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-06-05 05:59:34 +0800

    fix :-1:

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@
  * @Author: maggot-code
  * @Date: 2022-03-26 21:15:17
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-04-28 23:05:40
+ * @LastEditTime: 2022-06-05 05:15:07
  * @Description: file content
  */
 import { createApp } from 'vue';
-import { setupRouter } from "@neon-router/setup";
 
 import AppProxy from "@neon-layouts/AppProxy.vue";
 import App from "@neon-layouts/App.vue";
@@ -19,8 +18,6 @@ async function main() {
     const app = createApp(App);
 
     appProxy.mount('#app-proxy');
-
-    await setupRouter(app);
 
     app.mount('#app');
 }
--- src/services/network/http/http.ts
+++
@@ -1,7 +0,0 @@
-/*
- * @Author: maggot-code
- * @Date: 2022-05-08 16:51:11
- * @LastEditors: maggot-code
- * @LastEditTime: 2022-05-18 21:09:36
- * @Description: https://api.thecatapi.com/v1/images/search?limit=1
- */
