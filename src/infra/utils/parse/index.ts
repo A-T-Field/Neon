34a9697777def2a6f3978076ba9a32b2c0e19f87
maggot_code <63178315+maggot-code@users.noreply.github.com>
2022-06-29 00:43:08 +0800

    fix :-1:

--- src/infra/utils/parse/index.ts
+++ src/infra/utils/parse/index.ts
@@ -1,11 +1,11 @@
 /*
- * @FilePath: \Neon\src\infra\utils\parse\index.ts
+ * @FilePath: /Neon/src/infra/utils/parse/index.ts
  * @Author: maggot-code
  * @Date: 2022-06-28 13:56:13
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-06-28 14:05:25
+ * @LastEditTime: 2022-06-28 23:37:16
  * @Description:
  */
-export function toParse(str: string): any {
-    console.log(str);
+export function toParse(value: string) {
+    console.log(value);
 }
--- test/parse.test.ts
+++ test/parse.test.ts
@@ -1,16 +1,17 @@
 /*
- * @FilePath: \Neon\test\parse.test.ts
+ * @FilePath: /Neon/test/parse.test.ts
  * @Author: maggot-code
  * @Date: 2022-06-28 13:56:52
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-06-28 14:06:55
+ * @LastEditTime: 2022-06-28 23:47:07
  * @Description:
  */
-import { describe, expect, it } from 'vitest';
+import { test, expect } from 'vitest';
 import { toParse } from '@neon/infra/utils/parse';
 
-describe('expect', () => {
-    it('parse', () => {
-        expect(toParse).toMatchInlineSnapshot(``);
-    });
+/**
+ * @example "@request()"
+ */
+test('to parse @request', () => {
+    expect(true).toBe(true);
 });
