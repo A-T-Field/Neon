6f55e15066ec6982617c216b34d081b162f456c5
maggot_code <wowhuanhuan233@163.com>
2022-07-19 18:10:43 +0800

    fix :-1:

--- src/shared/form/README.md
+++ src/shared/form/README.md
@@ -1,1 +1,31 @@
 # form
+
+## 布尔值对象
+```javascript
+export abstract class BooleanValueObject {
+    value:boolean;
+    setupValue:(val:boolean):void;
+}
+
+export function useBooleanValueObject(vok:string,state?:boolean):BooleanValueObject{
+    const valueRef = ref(state);
+
+    function setupValue(val:boolean):void{
+        valueRef.value = val;
+    }
+
+    return {
+        value:unref(valueRef),
+    }
+}
+```
+
+## Field
+1. 初始化状态
+2. 挂载状态
+3. 卸载状态
+4. 加载状态
+5. 执行校验状态
+6. 校验结果状态
+7. 激活状态
+8. 浏览过状态
--- src/shared/layouts/App.vue
+++ src/shared/layouts/App.vue
@@ -2,12 +2,20 @@
  * @Author: maggot-code
  * @Date: 2022-04-19 23:57:05
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-07-19 17:10:00
+ * @LastEditTime: 2022-07-19 17:32:18
  * @Description: file content
 -->
-<script setup lang='ts'></script>
+<script setup lang='ts'>
+import { useBooleanValueObject } from "@neon-form/Usecase";
+const { value,setupValue } = useBooleanValueObject("init", false);
+</script>
 
-<template>App</template>
+<template>
+<h1>App</h1>
+
+<h2>{{value}}</h2>
+<button @click="value ? setupValue(false) : setupValue(true)">修改</button>
+</template>
 
 <style scoped lang='scss'>
 </style>
