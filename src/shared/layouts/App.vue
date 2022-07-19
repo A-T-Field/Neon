22679660069eedf3adca50c56703b72d0f35f5f2
maggot_code <wowhuanhuan233@163.com>
2022-07-19 17:10:11 +0800

    Update App.vue

--- src/shared/layouts/App.vue
+++ src/shared/layouts/App.vue
@@ -2,34 +2,12 @@
  * @Author: maggot-code
  * @Date: 2022-04-19 23:57:05
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-07-18 15:32:06
+ * @LastEditTime: 2022-07-19 17:10:00
  * @Description: file content
 -->
-<script setup lang='ts'>
-const pwd = ref("");
-const usablepwd = ref("");
-function handlerOutput() {
-    // fe.status.setInitialized(true);
-    // console.log(
-    //     unref(pwd),
-    //     unref(usablepwd)
-    // );
-}
-</script>
+<script setup lang='ts'></script>
 
-<template>
-    <form>
-        密码：<input type="text"  v-model="pwd">
-        确认密码：<input type="text" v-model="usablepwd">
-        <button type="button" @click="handlerOutput">提交</button>
-        <div>
-            <p>密码输出：{{pwd}}</p>
-            <p>确认密码输出：{{usablepwd}}</p>
-        </div>
-        <!-- <h1>字段初始化状态：{{fe.status.initialized.value}}</h1>
-        <h1>字段初始化描述：{{fe.status.initialized.label}}</h1> -->
-    </form>
-</template>
+<template>App</template>
 
 <style scoped lang='scss'>
 </style>
