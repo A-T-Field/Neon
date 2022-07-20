248a91c00d314d60b7c221f5eab3c7006020bce5
maggot_code <wowhuanhuan233@163.com>
2022-07-20 09:56:41 +0800

    fix :-1:

--- src/shared/layouts/App.vue
+++ src/shared/layouts/App.vue
@@ -2,19 +2,14 @@
  * @Author: maggot-code
  * @Date: 2022-04-19 23:57:05
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-07-19 17:32:18
+ * @LastEditTime: 2022-07-20 09:56:17
  * @Description: file content
 -->
 <script setup lang='ts'>
-import { useBooleanValueObject } from "@neon-form/Usecase";
-const { value,setupValue } = useBooleanValueObject("init", false);
 </script>
 
 <template>
 <h1>App</h1>
-
-<h2>{{value}}</h2>
-<button @click="value ? setupValue(false) : setupValue(true)">修改</button>
 </template>
 
 <style scoped lang='scss'>
