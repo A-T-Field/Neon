f968ab2c3747cd7874c93c2b7d65825f1801618a
maggot_code <wowhuanhuan233@163.com>
2022-07-22 10:57:10 +0800

    fix :-1:

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,18 @@
  * @Author: maggot-code
  * @Date: 2022-03-26 21:15:17
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-07-22 10:52:33
+ * @LastEditTime: 2022-07-22 10:54:28
  * @Description: file content
  */
 import { createApp } from 'vue';
 
-import AppProxy from '@neon-layouts/AppProxy.vue';
 import App from '@neon-layouts/App.vue';
 
 import 'normalize.css';
 import '@neon/assets/style/index.scss';
 
 async function main() {
-    const appProxy = createApp(AppProxy);
     const app = createApp(App);
-
-    appProxy.mount('#app-proxy');
-
     app.mount('#app');
 }
 
